2d6ae574814a7784c33ccf49b9e2a2eeaff7e4df
livetofight <xoagimay@gmail.com>
2020-03-10 11:31:06 +0700

    chinh sua URL client vs admin

---
+++ app/Http/Controllers/ExamController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExamController extends Controller
+{
+    public function index()
+    {
+        return view('admin.exam.exam',[ 'title' => 'Danh Sách Đề Thi']);
+    }
+
+    public function listexam()
+    {
+        return view('admin.exam.listexam',[ 'title' => 'Danh Sách Thi']);
+    }
+}
